8489e615a9713546c5e7ede5b45a2a3656f0c706
SedAtmaca <53580694+SedAtmaca@users.noreply.github.com>
2020-03-14 11:09:23 +0300

    Küçük Değişiklikler

---
+++ templates/fundamentals/footer.js
@@ -0,0 +1,14 @@
+document.write(`<div class="footer">
+      <div class="f-middle">
+        <p style="font-size:40px;">Follow Us</p>
+        <center><a href="#"><i class="fab fa-facebook-square"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-linkedin"></i></a>
+          <a href="#"><i class="fab fa-github"></i></a>
+          <a href="#"><i class="fab fa-reddit"></i></a>
+          <center>
+            <div class="border"></div>
+            <p>2020 © Created by SedAtmaca & CosmicOwl All Rights Reserved</p>
+      </div>
+    </div>`);
---
+++ templates/fundamentals/header.js
@@ -0,0 +1,14 @@
+document.write(`<div class="mid-header">
+        <div class="logo">
+          <a href="index.html">OurMood</a>
+        </div>
+        <ul class="nav-links">
+          <a href="article.html">Articles</a>
+          <a href="add-article.html">Add Article</a>
+          <a href="about.html">About</a>
+          <a href="contact.html">Contact</a>
+          <a href="sign-in.html"><button>Sign In</button></a>
+          <a href="sign-up.html"><button>Sign Up</button></a>
+
+        </ul>
+      </div>`);
